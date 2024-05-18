f45a46eaf859020133d66de9fadf6f7b38fef21e
Caner Uçar <canerucar0@gmail.com>
2024-05-18 18:42:35 +0300

    fix : Comment added to api line

--- app/components/api/index.js
+++ app/components/api/index.js
@@ -1,4 +1,5 @@
 export const getData = async () => {
+  //this API can be hidden
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await response.json();
   return data;
